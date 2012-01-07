d0200f2878e72107d3ae02bc410e84d688539f92
Mark Johnson <wmdmark@gmail.com>
2012-01-06 20:06:55 -0500

    script tweak

--- public/javascripts/parallax.coffee
+++ public/javascripts/parallax.coffee
@@ -4,7 +4,6 @@ $(document).ready ->
 		bannerHeight = 500
 		scroll = $(window).scrollTop()
 		offset = (scroll/height) * 220
-		console.log "offset: #{offset}"
 		bannerVerticalPos = -(bannerHeight/2) + offset
 		$("header").css
 			'background-position': "50% #{bannerVerticalPos}px"
--- public/javascripts/parallax.js
+++ public/javascripts/parallax.js
@@ -8,7 +8,6 @@
       bannerHeight = 500;
       scroll = $(window).scrollTop();
       offset = (scroll / height) * 220;
-      console.log("offset: " + offset);
       bannerVerticalPos = -(bannerHeight / 2) + offset;
       return $("header").css({
         'background-position': "50% " + bannerVerticalPos + "px"
