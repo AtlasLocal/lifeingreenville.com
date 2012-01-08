8004c1ed9d96b888b2aaa889c6f6c5c773ceeefc
Mason Stewart <masondesu@gmail.com>
2012-01-08 12:51:53 -0500

    Commented out console.log calls

--- public/javascripts/parallax.js
+++ public/javascripts/parallax.js
@@ -16,9 +16,9 @@
       }
       bgHeight = bannerHeight * 1.2;
       offset = ($(window).scrollTop() / $(document).height()) * 220;
-      console.log("50% " + bannerVerticalPos + "px");
+      // console.log("50% " + bannerVerticalPos + "px");
       bannerVerticalPos = Math.floor(((bannerHeight - bgHeight) / 2) + offset);
-      console.log("bannerVerticalPos:", "50% " + bannerVerticalPos + "px");
+      // console.log("bannerVerticalPos:", "50% " + bannerVerticalPos + "px");
       return $("header").css({
         'background-position': "50% " + bannerVerticalPos + "px"
       });
