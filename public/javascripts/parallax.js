5a6406e0ab58b63414ad11c0f47505bfa3539fab
Mark Johnson <wmdmark@gmail.com>
2012-01-09 16:33:06 -0500

    smoother parallax?

--- public/javascripts/parallax.js
+++ public/javascripts/parallax.js
@@ -1,32 +1,36 @@
 (function() {
+
   $(document).ready(function() {
-    var scrollBannerBG;
-    if (detect === 'iPhone') {
-      return;
-    }
-    scrollBannerBG = function() {
+    var animate, scrollBannerBG;
+    if (detect === 'iPhone') return;
+    scrollBannerBG = function(animate) {
       var bannerHeight, bannerVerticalPos, bgHeight, offset, width;
+      if (animate == null) animate = true;
       width = $(window).width();
       bannerHeight = 560;
-      if (width <= 1023) {
-        bannerHeight = 500;
-      }
-      if (width <= 500) {
-        bannerHeight = 400;
-      }
+      if (width <= 1023) bannerHeight = 500;
+      if (width <= 500) bannerHeight = 400;
       bgHeight = bannerHeight * 1.2;
       offset = ($(window).scrollTop() / $(document).height()) * 500;
-      bannerVerticalPos = (((bannerHeight - bgHeight) / 2) + offset);
-      return $("header").css({
-        'background-position': "50% " + bannerVerticalPos + "px"
-      });
+      bannerVerticalPos = ((bannerHeight - bgHeight) / 2) + offset;
+      if (animate) {
+        return $("header").stop().animate({
+          'background-position-y': "" + bannerVerticalPos
+        }, 60, 'easeOutQuad');
+      } else {
+        return $("header").css({
+          'background-position': "50% " + bannerVerticalPos + "px"
+        });
+      }
     };
     $(window).bind("scroll", function(e) {
-      return scrollBannerBG();
+      var animate;
+      return scrollBannerBG(animate = true);
     });
     $(window).bind('resize', function(e) {
       return scrollBannerBG();
     });
-    return scrollBannerBG();
+    return scrollBannerBG(animate = false);
   });
+
 }).call(this);
