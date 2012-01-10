79975c910ca11d94dcfe74faf71af404b221def6
Sean Gaffney <seangaffney@gmail.com>
2012-01-09 22:55:59 -0500

    Footer slide adjustments

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -21,11 +21,11 @@ $(document).ready(function() {
     $('#footer_nav').hide();
     
     $('#toggle').toggle(function(e) {
-        $('#footer_nav').slideDown(800);
-        $.scrollTo('+='+footerHeight+'px', 800);
+        $('#footer_nav').slideDown(500);
+        $.scrollTo('+='+footerHeight+'px', 500, {easing: 'swing'});
         $(this).removeClass('up').addClass('down');
       }, function(e) {
-        $('#footer_nav').slideUp(300);
+        $('#footer_nav').slideUp(300, 'swing');
         $(this).removeClass('down').addClass('up')
       }
     );
